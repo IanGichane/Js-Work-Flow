a55693ad1313c5c5390d46d7c67912eb4810d1e9
Ian <gichaneian@gmail.com>
2023-09-13 10:24:18 +0300

    New additions

---
+++ Ternaryop.js
@@ -0,0 +1,26 @@
+// statement?truthy:Falsy
+
+/**
+ * 
+ * 
+ * let isRaining = true
+isRaining
+  ? console.log('You need a rain coat.')  : console.log('No need for a rain coat.')
+
+
+
+isRaining = false
+isRaining
+  ? console.log('You need a rain coat.'): console.log('No need for a rain coat.')
+
+ */
+
+  //TERNARY OPERATOR
+  //const age = 15
+  //const canVote = age>18 ?'can vote':'cannot vote';
+  //console.log(canVote)
+
+
+  const Age = 25
+  const canVote = Age>18 ? 2+3+'5':'cannot vote';
+  console.log(canVote)
---
+++ sequence.js
@@ -0,0 +1,33 @@
+//Sequence
+//Selction-We can choose to run certain codes and not others
+//we can choose to do something untill a certain condtion is met
+
+/**
+ * if(age >18){
+    console.log('CAn vote')
+}else{
+    console.log('Cannot vote')
+}
+
+ */
+
+const user='student'
+const usertype='something else'
+if (user==='student'){
+    if(usertype=='something else'){
+        console.log('Student/TM logged in')
+    }else{
+        console.log('Student logged in')
+    }
+}else if(user==='TM'){
+    console.log('TM logged in')
+}else if (user==='pod'){
+    console.log('Pod lead logged in')
+}
+
+//SWITCH CASE
+switch(User){
+    case'student';
+    console
+
+}
