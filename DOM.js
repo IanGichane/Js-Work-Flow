cfb76458e2530426befdb9d2eb2c05b5078905d0
Ian <gichaneian@gmail.com>
2023-09-19 15:15:16 +0300

    My repo learn

---
+++ DOM.js
@@ -0,0 +1,39 @@
+/**
+ * DOM-Document object Modelling
+ * DOM is like human DNA,Any change to the dna has a significant change to to the outward appearance of the human body.
+ * Similarly,when you change something in the dom,you change what is displayed in the browser.
+ * Middle player btn the user and the underlying HTML/CSS/JS
+ * 
+ */
+//1.1 Accessing the DOM
+/**
+ * Can be accessed through two variables:window and document
+ * The window variable points to an object that represents Chrome's information about the browser
+ */
+//1.2 from youtube 
+/**
+ * DOM is a tree
+ * -it represents the content of a HTML DOC AS A TREE STRUCTURE
+ * -it is an API used with js
+ * Helps easily read,access and update the contents of the document
+ * 
+ * DOCUMENT -file
+ * OBJECT- tags and elements
+ * MODEL- layout structure
+ * 
+ * -->DOM is an object oriented representationof the web page ,which can be modified with a scripting language eg js
+ */
+
+//1.3tHE STRUCTURE OF DOM
+/**
+ * -Structured using nodes
+ * nodes= tags
+ * 
+ */
+/**
+ * Ask the DOM to find or select an HTML element or elements in the rendered page
+Remove the selected element(s) and/or insert new element(s)
+Adjust a property of the selected element(s)
+ */
+
+//1.4 QUERY SELECTOR METHODS
